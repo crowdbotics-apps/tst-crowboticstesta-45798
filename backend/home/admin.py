8bed90031057dd5aad32561d1d0c9cae6ec7e562
crowdbotics-apps <team@crowdbotics.com>
2024-02-02 12:22:21 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Dwfe
+
+admin.site.register(Dwfe)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,10 @@
+from django.conf import settings
 from django.db import models
+
+
+class Dwfe(models.Model):
+    "Generated Model"
+    sdv = models.BigIntegerField()
+    wer = models.CharField(
+        max_length=96,
+    )
